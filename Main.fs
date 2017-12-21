151e58eb9ee49a650d79cea7702e07ffd6cef8ef
lobrien <lobrien@knowing.net>
2017-12-21 13:44:27 -1000

    F# Advent Calendar Draft (asymmetrical Perlin threshold)

--- Main.fs
+++ Main.fs
@@ -1,4 +1,4 @@
-﻿namespace NoiseGameplayKit
+﻿namespace FunnyFace
 
 open UIKit
 
--- ViewController.fs
+++ ViewController.fs
@@ -1,48 +1,22 @@
-﻿namespace NoiseGameplayKit
+﻿namespace FunnyFace
 
 open System
 
 open Foundation
 open UIKit
-open ARKit
-open SceneKit
 
 [<Register ("ViewController")>]
 type ViewController (handle:IntPtr) =
     inherit UIViewController (handle)
 
-    let mutable arsceneview : ARSCNView = null
+    override x.DidReceiveMemoryWarning () =
+        // Releases the view if it doesn't have a superview.
+        base.DidReceiveMemoryWarning ()
+        // Release any cached data, images, etc that aren't in use.
 
-    let ConfigureAR() = 
-       let cfg = new ARFaceTrackingConfiguration()
-       cfg.LightEstimationEnabled <- true
-       cfg
-
-    override this.DidReceiveMemoryWarning () =
-      base.DidReceiveMemoryWarning ()
-
-    override this.ViewDidLoad () =
-      base.ViewDidLoad ()
-
-      match ARFaceTrackingConfiguration.IsSupported with
-      | false -> raise <| new NotImplementedException() 
-      | true -> 
-        arsceneview <- new ARSCNView()
-        arsceneview.Frame <- this.View.Frame
-        arsceneview.Delegate <- new ARDelegate(ARSCNFaceGeometry.CreateFaceGeometry(arsceneview.Device, false))
-        //arsceneview.DebugOptions <- ARSCNDebugOptions.ShowFeaturePoints + ARSCNDebugOptions.ShowWorldOrigin
-        arsceneview.UserInteractionEnabled <- true
-
-        this.View.AddSubview arsceneview
-
-    override this.ViewWillAppear willAnimate = 
-        base.ViewWillAppear willAnimate
-
-        // Configure ARKit 
-        let configuration = new ARFaceTrackingConfiguration()
-
-        // This method is called subsequent to `ViewDidLoad` so we know arsceneview is instantiated
-        arsceneview.Session.Run (configuration , ARSessionRunOptions.ResetTracking ||| ARSessionRunOptions.RemoveExistingAnchors)
+    override x.ViewDidLoad () =
+        base.ViewDidLoad ()
+        // Perform any additional setup after loading the view, typically from a nib.
 
     override x.ShouldAutorotateToInterfaceOrientation (toInterfaceOrientation) =
         // Return true for supported orientations
