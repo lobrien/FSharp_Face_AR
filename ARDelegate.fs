04b8a6c0272bb4089d39d0753f208d35097b7e33
lobrien <lobrien@knowing.net>
2017-12-21 13:47:19 -1000

    Switch projectname

--- ARDelegate.fs
+++ ARDelegate.fs
@@ -1,1 +1,22 @@
-﻿
+﻿namespace FunnyFace
+
+open ARKit
+open SceneKit
+   
+// Delegate object for AR: called on adding and updating nodes
+type ARDelegate(faceGeometry : ARSCNFaceGeometry) =
+   inherit ARSCNViewDelegate()
+  
+   // The geometry to overlay on top of the ARFaceAnchor (recognized face)
+   let faceNode = new Mask(faceGeometry)
+
+   override this.DidAddNode (renderer, node, anchor) = 
+      match anchor <> null && anchor :? ARFaceAnchor with 
+      | true -> node.AddChildNode faceNode
+      | false -> ignore()   
+
+   override this.DidUpdateNode (renderer, node, anchor) = 
+
+      match anchor <> null && anchor :? ARFaceAnchor with 
+      | true -> faceNode.Update (anchor :?> ARFaceAnchor)
+      | false -> ignore()
--- ViewController.fs
+++ ViewController.fs
@@ -4,19 +4,43 @@ open System
 
 open Foundation
 open UIKit
+open ARKit
+open SceneKit
 
 [<Register ("ViewController")>]
 type ViewController (handle:IntPtr) =
     inherit UIViewController (handle)
 
-    override x.DidReceiveMemoryWarning () =
-        // Releases the view if it doesn't have a superview.
-        base.DidReceiveMemoryWarning ()
-        // Release any cached data, images, etc that aren't in use.
+    let mutable arsceneview : ARSCNView = new ARSCNView()
 
-    override x.ViewDidLoad () =
-        base.ViewDidLoad ()
-        // Perform any additional setup after loading the view, typically from a nib.
+    let ConfigureAR() = 
+       let cfg = new ARFaceTrackingConfiguration()
+       cfg.LightEstimationEnabled <- true
+       cfg
+
+    override this.DidReceiveMemoryWarning () =
+      base.DidReceiveMemoryWarning ()
+
+    override this.ViewDidLoad () =
+      base.ViewDidLoad ()
+
+      match ARFaceTrackingConfiguration.IsSupported with
+      | false -> raise <| new NotImplementedException() 
+      | true -> 
+        arsceneview.Frame <- this.View.Frame
+        arsceneview.Delegate <- new ARDelegate (ARSCNFaceGeometry.CreateFaceGeometry(arsceneview.Device, false))
+        //arsceneview.DebugOptions <- ARSCNDebugOptions.ShowFeaturePoints + ARSCNDebugOptions.ShowWorldOrigin
+
+        this.View.AddSubview arsceneview
+
+    override this.ViewWillAppear willAnimate = 
+        base.ViewWillAppear willAnimate
+
+        // Configure ARKit 
+        let configuration = new ARFaceTrackingConfiguration()
+
+        // This method is called subsequent to `ViewDidLoad` so we know arsceneview is instantiated
+        arsceneview.Session.Run (configuration , ARSessionRunOptions.ResetTracking ||| ARSessionRunOptions.RemoveExistingAnchors)
 
     override x.ShouldAutorotateToInterfaceOrientation (toInterfaceOrientation) =
         // Return true for supported orientations
